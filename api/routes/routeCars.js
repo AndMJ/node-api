44499142e9aaa6dbc0f496a07f9c739e46b7ac3e
AndMJ <andrejoao070@gmail.com>
2024-03-22 20:38:16 +0000

    UPDATE with .env vars
    ADD code comments

--- api/routes/routeCars.js
+++ api/routes/routeCars.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
+    //Load car controller
     const controllerCars = require("../controllers/controllerCars")();
 
-    app.route("/api/v1/cars/list").get(controllerCars.listCars)
-    app.route("/api/v1/cars/create").post(controllerCars.createCar)
+    //Declare car routes
+    app.route(app.get("API_URI") + "cars/list").get(controllerCars.listCars)
+    app.route(app.get("API_URI") + "cars/create").post(controllerCars.createCar)
 }
