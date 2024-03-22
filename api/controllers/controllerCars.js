edd571c5e216651d36705481213774c3cac77044
AndMJ <andrejoao070@gmail.com>
2024-03-22 20:37:54 +0000

    ADD code comments

--- api/controllers/controllerCars.js
+++ api/controllers/controllerCars.js
@@ -11,22 +11,23 @@ module.exports = () => {
                 cars: result
             })
         } catch (error) {
-            res.status(401).json({
-                message: error
+            //also catches error thrown by model
+            res.status(400).json({
+                message: error.message
             })
         }
     }
 
     controller.createCar = async (req, res) => {
         try {
-            //call modules
+            //formulate and validate car data json from request body
             const car = {
-                plate: req.body.plate.trim(),
+                license_plate: req.body.license_plate.trim(),
                 manufacturer: req.body.manufacturer.trim(),
                 model: req.body.model.trim(),
                 color: req.body.color.trim(),
             }
-
+            //call modules
             const result = await modelCars.createCar(car)
 
             res.status(200).json({
@@ -34,8 +35,9 @@ module.exports = () => {
                 car: result
             });
         } catch (error) {
-            res.status(401).json({
-                message: error
+            //also catches error thrown by model
+            res.status(400).json({
+                message: error.message
             });
         }
     }
