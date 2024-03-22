feb0a2d208f53faa27e09deef5bcacc10617709e
AndMJ <andrejoao070@gmail.com>
2024-03-22 00:15:03 +0000

    UPDATE server.js

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-const express = require("express")
-const app = express()
+const app = require("./config/express_config")
 
-const listUsers = async (req, res) => {
-    res.status(200).json({
-        users: "list of users"
-    })
-}
+app().listen(3000, () => console.log("Server started!"))
 
-app.listen(3000, () => console.log("Server started!"))
 
-app.route("/api/v1/users/list").get(listUsers)
