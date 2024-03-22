9638d93ac758a5815238bbd7285db083c369a3d0
AndMJ <andrejoao070@gmail.com>
2024-03-22 20:37:43 +0000

    UPDATE with .env vars
    ADD code comments

--- config/express_config.js
+++ config/express_config.js
@@ -1,15 +1,23 @@
-const express = require("express")
+const express = require("express");
 const bodyParser = require('body-parser');
+
+require("dotenv").config()
 
 module.exports = () => {
     const app = express();
 
-    //Set middleware
+    //Set middleware (It's what's used in the communication between client and API, API and DB)
     //make it so api receives/responds using json, so we can get the request body data
     app.use(bodyParser.json());//content-type - application/json
 
-    //Load routes
+    //configs
+    app.set("port", process.env.API_PORT)
+    app.set("API_URI", process.env.API_URI)
+
+    //Load all app routes
     require("../api/routes/routeCars")(app);
+    //require("../api/routes/routeUsers")(app);
+    //require("../api/routes/routeCarManufacturers")(app);
 
     return app;
 }
